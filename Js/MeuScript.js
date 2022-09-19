e13fa585634dcd0b44719821d0e75b3b309bb5f8
Everson do Nascimento Silva <everson25nascimento@gmail.com>
2022-09-19 16:08:20 -0300

    Consertando Imagens

--- Js/MeuScript.js
+++ Js/MeuScript.js
@@ -1,30 +1,26 @@
-
 let numero = 1;
 
-	function adicionar() {
-		if (numero >=3) {
-			numero = 1;
-			document.getElementById('slide').src ='imagens/mais_avaliado/'+ numero + '.png';
-		}else{
-			numero += 1;
-				
-			document.getElementById('slide').src = 'imagens/mais_avaliado/'+ numero + '.png';
-			
-		}
-		setTimeout(adicionar,2000);
-	}
+function adicionar() {
+  if (numero >= 3) {
+    numero = 1;
+    document.getElementById("slide").src =
+      "../Imagens/mais_avaliado/" + numero + ".png";
+  } else {
+    numero += 1;
 
-	adicionar()
+    document.getElementById("slide").src =
+      "../Imagens/mais_avaliado/" + numero + ".png";
+  }
+  setTimeout(adicionar, 2000);
+}
 
-	function mouse(){
+adicionar();
 
-		document.getElementById('slide').style.boxShadow = "3px 3px 10px black" ;
-		document.getElementById('slide').style.transition = "box-shadow 1s";
+function mouse() {
+  document.getElementById("slide").style.boxShadow = "3px 3px 10px black";
+  document.getElementById("slide").style.transition = "box-shadow 1s";
+}
 
-	}
-
-	function mouseDown(){
-
-		document.getElementById('slide').style.boxShadow = "none" ;
-
+function mouseDown() {
+  document.getElementById("slide").style.boxShadow = "none";
 }
